34deb25f2482690596122627c5160411c7f0a11c
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-01-25 10:47:22 +0100

    take the namespace

---
+++ app/index.js
@@ -0,0 +1,10 @@
+
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+
+    firstThink: function () {
+        this.log('This generator is under development...');
+    },
+
+});
