c24b09ac8f3f4b3d0366fb56cdc42e81038343e9
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-01-27 20:49:40 +0100

    style guide source refactor

--- component/templates/Component.guide.js
+++ component/templates/Component.guide.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import Grid from 'react-bootstrap/lib/Grid';
-import PageHeader from 'react-bootstrap/lib/PageHeader';
+import { GuidePage, GuideSection } from '../index';
+
 
 import { <%= componentName %> } from 'components/<%= componentName %>';
 
 export default class <%= componentName %>GuideComponent extends React.Component {
     render() {
         return (
-            <Grid>
-                <PageHeader><%= componentName %> <small>whithout propertied</small></PageHeader>
-                <<%= componentName %> />
+            <GuidePage>
+                <GuideSection title="<%= componentName %> - whithout properties">
+                    <<%= componentName %> />
+                </GuideSection>
 
-                <PageHeader><%= componentName %> <small>with value</small></PageHeader>
-                <<%= componentName %> val="Content for the component" />
-            </Grid>
+                <GuideSection title="<%= componentName %> - with value">
+                    <<%= componentName %> val="Content for the component" />
+                </GuideSection>
+            </GuidePage>
         );
     }
 }
