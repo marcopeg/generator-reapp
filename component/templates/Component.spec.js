d8497ab60ef072e7bdecf00f6728d5c8d053cf3f
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-02-10 15:58:39 +0100

    refactor component spec scaffold

--- component/templates/Component.spec.js
+++ component/templates/Component.spec.js
@@ -8,8 +8,6 @@ import { <%= componentName %> } from 'components/<%= componentName %>';
 
 describe('<%= componentName %> Component', () => {
     it('should render without exceptions', () => {
-        var cmp = ReactTestUtils.renderIntoDocument(<<%= componentName %> value="<%= componentName %>" />);
-        var nod = ReactDOM.findDOMNode(cmp);
-        expect(nod.innerText).to.equal('<%= componentName %>');
+        ReactTestUtils.renderIntoDocument(<<%= componentName %> />);
     });
 });
