3645364e97610ef0d153e1570922ff3a426498cb
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-03-20 10:59:44 +0100

    export default for components and container - app template

--- app/templates/app-scaffold/app/client/containers/App.js
+++ app/templates/app-scaffold/app/client/containers/App.js
@@ -8,8 +8,21 @@ import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import Button from 'react-bootstrap/lib/Button';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
+const STYLES = {
+    grid: {
+        marginTop: 50,
+    },
+    jumbo: {
+        borderRadius: 0,
+        boxShadow: '2px 2px 5px #aaa',
+    },
+    icon: {
+        marginRight: 10,
+    },
+};
+
 @connect(s => s.app)
-export class App extends React.Component {
+export default class App extends React.Component {
 
     static propTypes = {
         title: React.PropTypes.string,
@@ -17,28 +30,15 @@ export class App extends React.Component {
 
     render() {
 
-        var styles = {
-            grid: {
-                marginTop: 50,
-            },
-            jumbo: {
-                borderRadius: 0,
-                boxShadow: '2px 2px 5px #aaa',
-            },
-            icon: {
-                marginRight: 10,
-            },
-        };
-
         var reappUrl = 'https://github.com/marcopeg/generator-reapp';
 
         return (
-            <Grid style={styles.grid}>
-                <Jumbotron style={styles.jumbo}>
+            <Grid style={STYLES.grid}>
+                <Jumbotron style={STYLES.jumbo}>
                     <h3>{this.props.title}</h3>
                     <p>This <i>React/Redux</i> app is ready to rock the world!</p>
                     <Button href={reappUrl} bsStyle="success" target="_blank">
-                        <Glyphicon glyph="hand-right" style={styles.icon} />
+                        <Glyphicon glyph="hand-right" style={STYLES.icon} />
                         Learn more about Reapp
                     </Button>
                 </Jumbotron>
--- app/templates/app-scaffold/app/tests/fixtures/initial-state-dev.fixture.js
+++ app/templates/app-scaffold/app/tests/fixtures/initial-state-dev.fixture.js
@@ -1,6 +1,12 @@
 
 import { INITIAL_STATE as APP_INITIAL_STATE } from 'reducers/app-reducer';
 
-export const DEV = {
-    app: APP_INITIAL_STATE,
+const INITIAL_STATE = {
+    app: {
+        ...APP_INITIAL_STATE,
+        // override properties from the APP reducer:
+        // title: 'my fixture title'
+    },
 };
+
+export default INITIAL_STATE;
